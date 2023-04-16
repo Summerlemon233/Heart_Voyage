ac25283801c50b80bf5013019358744a9399d26e
Summerlemon_233 <2200012710@stu.pku.edu.cn>
2023-04-16 21:38:11 +0800

    Ver0.2.4 Bug fixes.

--- lib/pages/island/XINXIE.dart
+++ lib/pages/island/XINXIE.dart
@@ -17,12 +17,13 @@ class _XINXIEState extends State<XINXIE> {
         foregroundColor: Colors.white,
         title: Text('详细信息'),
       ),
-      body: Container(
-        height: MediaQuery.of(context).size.height * 0.6,
-        padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-        margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
-        child: Center(
-          child: Text(
+      body: ListView(
+        children: [Container(
+
+          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+          margin: EdgeInsets.fromLTRB(30, 20, 30, 20),
+          child: Center(
+            child: Text(
               "可以选择将你的信件发送至北大心协“解忧杂货铺”收信箱"
                   "（是一个由学生发起的、匿名的线上写信渠道，"
                   "旨在为有困扰的同学提供一个温暖安全的、倾诉和寻求支持的平台），"
@@ -32,15 +33,17 @@ class _XINXIEState extends State<XINXIE> {
                   "北京大学学生心理协会（以下简称“北大心协”）"
                   "依托于北京大学学生心理健康教育与咨询中心，借助其丰富而优质的专业教师资源，"
                   "开展各种丰富精彩的专题活动，为心理学爱好者提供实践与体验的平台。"
-            ,style: TextStyle(fontSize: 20,color: Color.fromRGBO(215, 169, 83, 1)),),
-        ),
-        decoration: BoxDecoration(
+              ,style: TextStyle(fontSize: 20,color: Color.fromRGBO(215, 169, 83, 1)),),
+          ),
+          decoration: BoxDecoration(
             /*border: Border.all(
               color: Colors.brown,
               width: 3.0,
             ),
             borderRadius: BorderRadius.all(Radius.circular(20.0))),*/
-      ),
-    ));
+          ),
+        )],
+      )
+      );
   }
 }
